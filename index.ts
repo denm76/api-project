b71ed4718236497937ce74a41368af54351da2f7
denm76 <denm76@gmail.com>
2024-07-08 11:07:37 +0300

    api-project: modified index.ts using express Router from comments-api.ts

--- index.ts
+++ index.ts
@@ -2,13 +2,22 @@ import { initDataBase } from "./src/services/db";
 import { Express } from "express";
 import { Connection } from "mysql2/promise";
 import { initServer } from "./src/services/server";
+import { commentsRouter } from "./src/api/comments-api";
 
 export let server: Express;
 export let connection: Connection | null;
 
+const ROOT_PATH = "/api";
+
 async function launchApplication() {
   server = initServer();
   connection = await initDataBase();
+
+  initRouter();
+}
+
+function initRouter(){
+  server.use(`${ROOT_PATH}/comments`, commentsRouter);
 }
 
 launchApplication();
